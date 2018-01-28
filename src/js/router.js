7b7c38ef569b415495c80e183b73a15ed1a9ab0d
dadada.dev <dadada80.dev@gmail.com>
2018-01-28 17:53:29 +0900

    clean code

--- src/js/router.js
+++ src/js/router.js
@@ -1,31 +1,6 @@
 import { $ } from './util';
-// import * as $dom from './dom';
 import { FetchList, FetchPost } from './fetch';
 
-// const showFront = () => {
-//   $('topBar').classList.add('topBar-hidden');
-//   $('innerContainer').classList.remove('innerContainer-listView', 'innerContainer-postView', 'innerContainer-errorView');
-//   $('bottomBar').classList.remove('bottomBar-listView');
-// };
-// const showList = () => {
-//   $('topBar').classList.remove('topBar-hidden');
-//   $('innerContainer').classList.add('innerContainer-listView');
-//   $('innerContainer').classList.remove('innerContainer-postView', 'innerContainer-errorView');
-//   $('bottomBar').classList.add('bottomBar-listView');
-// };
-// const showPost = () => {
-//   $('topBar').classList.remove('topBar-hidden');
-//   $('innerContainer').classList.add('innerContainer-postView');
-//   $('innerContainer').classList.remove('innerContainer-listView', 'innerContainer-errorView');
-//   $('bottomBar').classList.add('bottomBar-listView');
-// };
-// const showError = () => {
-//   $('errorView_textArea').innerText = 'このURLのコンテンツは存在しません。';
-//   $('topBar').classList.add('topBar-hidden');
-//   $('innerContainer').classList.add('innerContainer-errorView');
-//   $('innerContainer').classList.remove('innerContainer-listView', 'innerContainer-postView');
-//   $('bottomBar').classList.remove('bottomBar-listView');
-// };
 const showView = (state) => {
   if (state.view === 'front') {
     $('innerContainer').classList.remove('innerContainer-listView', 'innerContainer-postView', 'innerContainer-errorView');
@@ -65,27 +40,11 @@ const bottomBarBehavior = (state) => {
   if (state.view === 'front') $('bottomBar').classList.remove('bottomBar-listView');
   else if (state.view === 'list') $('bottomBar').classList.add('bottomBar-listView');
   else if (state.view === 'post') $('bottomBar').classList.add('bottomBar-listView');
-  else if (state.view === '404') {
-    // $('bottomBar').classList.remove('bottomBar-listView');
-    $('bottomBar_navItems').classList.add('bottomBar_navItems-error');
-  }
+  else if (state.view === '404') $('bottomBar_navItems').classList.add('bottomBar_navItems-error');
 };
+
 export default (state) => {
   showView(state);
   topBarBehavior(state);
   bottomBarBehavior(state);
-  // if (state.view === 'front') {
-  //   showFront();
-  // } else if (state.view === 'list') {
-  //   const getData = new FetchList(state);
-  //   getData.setData();
-  //   showList();
-  // } else if (state.view === 'post') {
-  //   const getData = new FetchPost(state);
-  //   getData.setData();
-  //   showPost();
-  // } else if (state.view === '404') {
-  //   showError();
-  //   bottomBarError();
-  // }
 };
